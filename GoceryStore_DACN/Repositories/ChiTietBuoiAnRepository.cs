7155e1c9f59e6c9b9348c227093b417896acc6d0
Datnp1810 <147915990+Datnp1810@users.noreply.github.com>
2024-11-13 21:08:25 +0700

    Up to main

---
+++ GoceryStore_DACN/Repositories/ChiTietBuoiAnRepository.cs
@@ -0,0 +1,54 @@
+﻿using GoceryStore_DACN.Data;
+using GoceryStore_DACN.Entities;
+using GoceryStore_DACN.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoceryStore_DACN.Repositories
+{
+    public class ChiTietBuoiAnRepository : IChiTietBuoiAnRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ChiTietBuoiAnRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CT_BuoiAn>> CreateCT_BuoiAn(List<CT_BuoiAn> ct_BuoiAn)
+        {
+            await _context.CTBuoiAns.AddRangeAsync(ct_BuoiAn);
+            await _context.SaveChangesAsync();
+            return ct_BuoiAn;
+        }
+
+        public async Task<bool> DeleteCT_BuoiAn(int ID_ThucPham, int ID_MonAn)
+        {
+            var timCT = await _context.CTBuoiAns.FirstOrDefaultAsync(p => p.ID_MonAn == ID_MonAn && p.ID_ThucPham == ID_ThucPham);
+            if (timCT != null) 
+            {
+                _context.CTBuoiAns.Remove(timCT);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+                  
+        }
+
+        public async Task<IEnumerable<CT_BuoiAn>> GetAllCT_BuoiAn()
+        {
+            return await _context.CTBuoiAns!.Include(p=>p.ID_MonAn)
+                .Include(l => l.ID_CDA)
+                .Include(q=>q.ID_MonAn).ToListAsync();
+        }
+
+        public Task<List<CT_BuoiAn>> GetAllCT_BuoiAnById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CT_BuoiAn> UpdateCT_BuoiAn(CT_BuoiAn ct_BuoiAn)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GoceryStore_DACN/Repositories/Interface/IChiTietBuoiAnRepository.cs
@@ -0,0 +1,13 @@
+﻿using GoceryStore_DACN.Entities;
+
+namespace GoceryStore_DACN.Repositories.Interface
+{
+    public interface IChiTietBuoiAnRepository
+    {
+        public Task<IEnumerable<CT_BuoiAn>> GetAllCT_BuoiAn();
+        public Task<List<CT_BuoiAn>> GetAllCT_BuoiAnById(int id);
+        public Task<List<CT_BuoiAn>> CreateCT_BuoiAn(List<CT_BuoiAn> ct_BuoiAn);
+        public Task<CT_BuoiAn> UpdateCT_BuoiAn(CT_BuoiAn ct_BuoiAn);
+        public Task<bool> DeleteCT_BuoiAn(int ID_ThucPham, int ID_MonAn);
+    }
+}
